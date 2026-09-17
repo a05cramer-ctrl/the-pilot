395874980acdb3b96c489dcd3e9d026dfbc9df40
a05cramer-ctrl <a05cramer@gmail.com>
2026-09-17 19:56:07 +0200

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 // THE PILOT · $PILOT
 window.PILOT_CFG = {
-  CA: "",
+  CA: "6eoBZcVMmHKyA4NgKF9HJ4PnJcheDZtdaXHC8rGbpump",
   CHAIN: "solana",
   PAD: "pumpfun",
   X: "https://x.com/Thepilotdrunk",
